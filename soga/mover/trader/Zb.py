7d6574420404b24f0ec33cd5df91984bb2c82778
idoplay <273789201@qq.com>
2018-01-28 20:57:52 +0800

    update

--- soga/mover/trader/Zb.py
+++ soga/mover/trader/Zb.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
-import urllib
-import math
 from mover.core.ZbApi import *
 
 
 class ZbTrader(ZbApi):
 
     def __init__(self):
-        SpiderEngine.__init__(self)
+        ZbApi.__init__(self)
 
     def account_info(self):
         self.query_account()
